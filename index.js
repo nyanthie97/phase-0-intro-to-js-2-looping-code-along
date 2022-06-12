9aaf699648ee7136b5eb2dbd54b5167eef128a87
Nyanthie Mayang Gatluak <nyanthie.mayang@student.moringaschool.com>
2022-06-12 16:54:59 +0300

    commit tree 825f29c96e789645a17479a540c04945eeac0367

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(stringName, eventName) {
+    const messages = []
+    for (let i = 0; i < stringName.length; i++) {
+        messages.push(`Thank you, ${stringName[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messages;
+
+}
+
+function countDown(integer){
+    while (integer > -1) {
+        console.log(integer--)
+    }
+
+} 
